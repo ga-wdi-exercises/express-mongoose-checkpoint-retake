737fd8d82a7f1c7a62f2d5db4565b0b24968cc44
Felix Sawicki <fmsawicki@gmail.con>
2018-10-15 14:28:07 -0400

    retake

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-// partials can be used to complete this checkpoint, they are not required though. 
+// partials can be used to complete this checkpoint, they are not required though.
 // in order to use partials you can use hbs to register the partials directory
 // setting a partial directory was covered in a previous exercise
 // partials have been provided in views/partials/
-
+const express = require("express");
+const app = express();
 
 //the below routes can be used or changed as you see fit
 app.get("/", (req, res) => {
- res.send('this should redirect to the /quests route')
-})
+  res.redirect("/quests");
+});
 
 app.get("/quests", (req, res) => {
-  res.send('this should show all the quests')
-})
+  res.send("this should show all the quests");
+});
 
 app.get("/quest/:id", (req, res) => {
-  res.send('this should show a single quest')
-})
+  res.send("this should show a single quest");
+});
 
-
-app.listen(3000, () => console.log('app is running'))
+app.listen(3000, () => console.log("app is running"));
 
 // DO NOT REMOVE THIS LINE:
-module.exports = app
-
+module.exports = app;
