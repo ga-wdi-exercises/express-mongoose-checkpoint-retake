4f400fa8bc0565f53bfcba4b9e16769df1416c16
Shakira Weir <shakira.weir87@gmail.com>
2019-01-29 14:27:18 -0500

    first route in checkpoint complete

--- index.js
+++ index.js
@@ -7,23 +7,20 @@ const app = express();
 const hbs = require("hbs");
 
 app.set("view engine", "hbs");
-//the below routes can be used or changed as you see fit
-// - `GET '/'` which should redirect to `/quests'
-// - `GET '/quests'` which render view with a list of notes
-// - `GET '/quest/:someParameter'` where `:someParameter` can be used to identify or grab a single `Quest` from the database.
+
 
 app.get("/", (req, res) => {
  res.redirect('/quests')
 })
 
-// app.get("/quests", (req, res) => {
-//   { Quest.find({}).sort({ createdAt: -1 }).then(quests => {res.render('index', { quests })
-// })
+app.get("/quests", (req, res) => {
+  res.send('this should show all the quests')
+})
 
 
-// app.get("/quest/:id", (req, res) => {
-//   { Quest.findOne({_id: req.params.id }).then(quest => { res.render('show', { quest })
-// })
+app.get("/quest/:id", (req, res) => {
+  res.send('this should show a single quest')
+})
 
 
 app.listen(3000, () => console.log('app is running'))
