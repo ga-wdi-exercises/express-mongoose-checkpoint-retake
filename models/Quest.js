4b1b73f79b53e9ccc825b91eb887aa34c22be97c
Vanessa Henderson <bookvanny@gmail.com>
2019-01-29 14:21:02 -0500

    checkpoint retake

--- models/Quest.js
+++ models/Quest.js
@@ -1,6 +1,16 @@
 const mongoose = require('../db/connection')
 
-const Quest = new mongoose.Schema({
+
+
+
+const Schema = mongoose.Schema;
+
+
+const Quest = new Schema({
+    title: String,
+    quest_giver: String,
+    rewards: Array,
+    locations: Array
 })
 
 module.exports = mongoose.model('Quest', Quest)
