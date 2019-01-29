a44decc4e4e6d6631bd0c49702a6d7196689fdc6
Courtney Tyler <courtneytyler7@gmail.com>
2019-01-29 14:20:12 -0500

    checkpoint code

--- index.js
+++ index.js
@@ -3,19 +3,27 @@
 // setting a partial directory was covered in a previous exercise
 // partials have been provided in views/partials/
 
+const express = require('express')
+const app = express()
+const hbs = require("hbs");
 
+app.set('view engine', 'hbs')
+
+const questController = require('./controllers/quests')
 //the below routes can be used or changed as you see fit
+
+// GET '/' which should redirect to `/quests'
 app.get("/", (req, res) => {
- res.send('this should redirect to the /quests route')
+ res.redirect('/quests')
 })
 
-app.get("/quests", (req, res) => {
-  res.send('this should show all the quests')
-})
+// GET '/quests' which render view with a list of notes
+app.get("/quests", questController.index)
 
-app.get("/quest/:id", (req, res) => {
-  res.send('this should show a single quest')
-})
+
+// GET '/quest/:someParameter' where :someParameter can be used to identify or grab a single Quest from the database.
+app.get("/quest/:id", questController.show)
+
 
 
 app.listen(3000, () => console.log('app is running'))
