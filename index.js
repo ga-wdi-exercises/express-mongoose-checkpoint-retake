c081a5de42c256a8ac1f7807a7ee25513c791118
Amare AbebeE <amaremark@gmail.com>
2019-01-29 14:32:04 -0500

    retest

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 // in order to use partials you can use hbs to register the partials directory
 // setting a partial directory was covered in a previous exercise
 // partials have been provided in views/partials/
-
+const express = require('express')
+const app = express()
+const hbs = require("hbs")
+const router = express.Router()
 
 //the below routes can be used or changed as you see fit
 app.get("/", (req, res) => {
